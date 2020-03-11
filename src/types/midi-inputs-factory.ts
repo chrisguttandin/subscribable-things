8afea2385cff7304d52ff31feaa57c4b854a3ca3
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-03-11 22:59:46 +0100

    fix: remove unnecessary todo comments

--- src/types/midi-inputs-factory.ts
+++ src/types/midi-inputs-factory.ts
@@ -1,5 +1,4 @@
 import { TMidiInputsFunction } from './midi-inputs-function';
 import { TWrapSubscribeFunctionFunction } from './wrap-subscribe-function-function';
 
-// @todo TypeScript does not include type definitions for the Web MIDI API yet.
 export type TMidiInputsFactory = (wrapSubscribeFunction: TWrapSubscribeFunctionFunction) => TMidiInputsFunction;
