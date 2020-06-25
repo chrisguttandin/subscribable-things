f87cc14e01bc910982881d036882f4a6991cd0e4
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-06-25 20:45:51 +0200

    build(config): filter tests by type

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -1,5 +1,11 @@
+const { env } = require('process');
+
+// eslint-disable-next-line padding-line-between-statements
+const filter = (predicate, ...tasks) => (predicate ? tasks : []);
+const isType = (...types) => env.TYPE === undefined || types.includes(env.TYPE);
+
 module.exports = {
     build: ['clean:build', 'sh:build-es2019', 'sh:build-es5'],
     lint: ['sh:lint-config', 'sh:lint-src', 'sh:lint-test'],
-    test: ['sh:test-integration', 'sh:test-unit']
+    test: ['build', ...filter(isType('integration'), 'sh:test-integration'), ...filter(isType('unit'), 'sh:test-unit')]
 };
