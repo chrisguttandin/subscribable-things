01b772f6ffb2a9d8f6d919967294d104d4453f26
Christoph Guttandin <chrisguttandin@media-codings.com>
2021-02-25 23:17:55 +0100

    fix: explicitly define entries as an array of IResizeObserverEntry

--- src/factories/resizes.ts
+++ src/factories/resizes.ts
@@ -1,3 +1,4 @@
+import { IResizeObserverEntry } from '../interfaces';
 import { TResizesFactory } from '../types';
 
 export const createResizes: TResizesFactory = (emitNotSupportedError, window, wrapSubscribeFunction) => {
@@ -7,7 +8,7 @@ export const createResizes: TResizesFactory = (emitNotSupportedError, window, wr
                 return emitNotSupportedError(observer);
             }
 
-            const resizeObserver = new window.ResizeObserver((entries) => observer.next(entries));
+            const resizeObserver = new window.ResizeObserver((entries) => observer.next(<IResizeObserverEntry[]>entries));
 
             try {
                 resizeObserver.observe(htmlElement, options);
