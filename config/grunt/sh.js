59bfe149f4a5afe2c3aac66ca8c67af1c1d534e6
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-03-20 23:55:18 +0100

    build(config): add test scripts

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,15 +1,13 @@
-module.exports = (grunt) => {
-    const continuous = grunt.option('continuous') === true;
-
+module.exports = () => {
     return {
         'build': {
             cmd: 'npm run build'
         },
         'test-integration': {
-            cmd: `karma start config/karma/config-integration.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:integration'
         },
         'test-unit': {
-            cmd: `karma start config/karma/config-unit.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:unit'
         }
     };
 };
